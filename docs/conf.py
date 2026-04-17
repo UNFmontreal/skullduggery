e2fcb64572a00b194ace18ae38bff8d766f57219
bpinsard <basile.pinsard@gmail.com>
2026-04-17 11:52:40 -0400

    fix docs conf metas

--- docs/conf.py
+++ docs/conf.py
@@ -19,9 +19,9 @@ sys.path.insert(0, os.path.abspath("../src/"))
 
 # -- Project information -----------------------------------------------------
 
-project = "ai-python docs"
-copyright = "2022, Daniel Ciborowski"
-author = "Daniel Ciborowski"
+project = "skullduggery docs"
+copyright = "2026, UNF Montréal"
+author = "Basile Pinsard"
 
 # The full version, including alpha/beta/rc tags
 release = "0.1.0"
